af7422a9789a0c2f4619c1feabccf527557342b3
Nouman <noumanmufc07@gmail.com>
2022-01-31 08:50:18 +0100

    format code and change max pooling filter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
+
 from helpers import *
+
 
 def create_arg_parser():
     parser = argparse.ArgumentParser()
@@ -11,11 +13,13 @@ def create_arg_parser():
         help="Model to train (lr: logistic regression, svm: support vector machine, cnn: convolutional neural network, mlp: multi-layer perceptron) (default: cnn)",
     )
     parser.add_argument(
-        "-d", "--data_augment", action="store_true", help="Whether to use data augmentation (default: False)",
+        "-d",
+        "--data_augment",
+        action="store_true",
+        help="Whether to use data augmentation (default: False)",
     )
     args = parser.parse_args()
     return args
-
 
 
 if __name__ == "__main__":
@@ -23,23 +27,39 @@ if __name__ == "__main__":
 
     data, labels = read_data("data/mfeat-pix.txt")
     X_train, y_train, X_test, y_test = split_data(data, labels)
-    
+
     if args.model_name == "lr":
         from sklearn.linear_model import LogisticRegression
+
         model = LogisticRegression()
 
-        fit_and_predict_model(model, "Logistic Regression", X_train, y_train, X_test, y_test, args.data_augment)
+        fit_and_predict_model(
+            model,
+            "Logistic Regression",
+            X_train,
+            y_train,
+            X_test,
+            y_test,
+            args.data_augment,
+        )
 
     elif args.model_name == "svm":
         from sklearn.svm import SVC
+
         model = SVC()
 
-        fit_and_predict_model(model, "Support Vector Machine", X_train, y_train, X_test, y_test, args.data_augment)
+        fit_and_predict_model(
+            model,
+            "Support Vector Machine",
+            X_train,
+            y_train,
+            X_test,
+            y_test,
+            args.data_augment,
+        )
 
     elif args.model_name == "cnn":
         fit_CNN_model(X_train, y_train, X_test, y_test, args.data_augment)
 
     else:
         fit_MLP_model(X_train, y_train, X_test, y_test, args.data_augment)
-
-    
